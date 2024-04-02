33841f3c393592140998320326540bd32797881d
Abhaya Kirtivasan <abhayak2k@gmail.com>
2024-04-02 14:55:42 -0400

    added first medium question

---
+++ Medium/threeSum.py
@@ -0,0 +1,20 @@
+def threeNumberSum(array, targetSum):
+    array.sort()
+    triplets = []
+    for i in range(len(array)):
+        left = i
+        right = len(array) - 1
+        while left < right:
+            currentSum = array[i] + array[left] + array[right]
+            if currentSum == targetSum:
+                triplets.append([array[i], array[left], array[right]])
+                left = left + 1
+                right = right - 1
+            elif currentSum < targetSum:
+                left = left + 1
+            elif currentSum > targetSum:
+                right = right - 1                
+    return triplets
+
+
+print(threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0))
