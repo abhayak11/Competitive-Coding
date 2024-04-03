dce788572d262f03eb553dbfe41dbf2432e644d8
Abhaya Kirtivasan <abhayak2k@gmail.com>
2024-04-03 17:24:19 -0400

    added hashtable approach

---
+++ Easy/twoNumberSumTwo.py
@@ -0,0 +1,13 @@
+def twoNumberSum(array, targetSum):
+    hashTableData = {}
+    for firstNum in array:
+        secondNum = targetSum - firstNum
+    
+        if secondNum in hashTableData:
+            return [firstNum, secondNum]
+
+        else:
+            hashTableData[firstNum] = True
+        
+
+print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
