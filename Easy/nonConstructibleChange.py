2a66cb3b9114d71576043bf4570e227c64849b12
Abhaya Kirtivasan <abhayak2k@gmail.com>
2024-04-10 17:32:44 -0400

    added a new code

---
+++ Easy/nonConstructibleChange.py
@@ -0,0 +1,15 @@
+def nonConstructibleChange(coins):
+    coins.sort()
+    currentChangeCreated = 0
+    for coin in coins:
+        if coin > currentChangeCreated + 1:
+            return currentChangeCreated + 1
+        currentChangeCreated += coin
+        
+    return currentChangeCreated+1
+
+    
+    return 1
+
+
+print(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]))
